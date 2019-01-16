d803cb024f12da48a57e3e24cd2a4ac7dfeb2d0c
Jan Stephan <j.stephan@hzdr.de>
2019-01-16 13:45:51 +0100

    HC, HIP: zcopy autotuning

--- examples/zcopy/rocm/hc/zcopy.cpp
+++ examples/zcopy/rocm/hc/zcopy.cpp
@@ -1,22 +1,29 @@
 #include <algorithm>
+#include <chrono>
 #include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <limits>
 #include <locale>
+#include <sstream>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
-#pragma clang diagnostic ignored "-Wc++17-extensions"
 #include <hc.hpp>
 #pragma clang diagnostic pop
 
 #include <hc_math.hpp>
-#include <hc_short_vector.hpp>
 
-constexpr auto elems = 1 << 25;
-constexpr auto iters = 10;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#pragma clang diagnostic ignored "-Wgnu-anonymous-struct"
+#include <hc_short_vector.hpp>
+#pragma clang diagnostic pop
 
 using double2 = hc::short_vector::double_2;
+using double4 = hc::short_vector::double_4;
 
 namespace
 {
@@ -34,18 +41,54 @@ namespace
         new cvt{sys_loc.name()}};
 }
 
-auto initialize(hc::tiled_index<1> idx, hc::array_view<double2, 1> A,
-                hc::array_view<double2, 1> B) [[hc]] -> void
+template <typename DataT>
+auto initialize(hc::tiled_index<1> idx, hc::array_view<DataT, 1> A,
+                hc::array_view<DataT, 1> B) [[hc]] -> void;
+
+template <>
+auto initialize<float>(hc::tiled_index<1> idx, hc::array_view<float, 1> A,
+                       hc::array_view<float, 1> B) [[hc]] -> void
 {
     auto i = idx.global[0];
-    A[i] = double2{0.0, 0.0};
+    A[i] = float{};
+    B[i] = std::numeric_limits<float>::quiet_NaN();
+}
+
+template <>
+auto initialize<double>(hc::tiled_index<1> idx, hc::array_view<double, 1> A,
+                       hc::array_view<double, 1> B) [[hc]] -> void
+{
+    auto i = idx.global[0];
+    A[i] = double{};
+    B[i] = std::numeric_limits<double>::quiet_NaN();
+}
+
+template <>
+auto initialize<double2>(hc::tiled_index<1> idx, hc::array_view<double2, 1> A,
+                         hc::array_view<double2, 1> B) [[hc]] -> void
+{
+    auto i = idx.global[0];
+    A[i] = double2{};
     B[i] = double2{std::numeric_limits<double>::quiet_NaN(),
                    std::numeric_limits<double>::quiet_NaN()};
 }
 
-auto read_write(hc::tiled_index<1> idx, hc::array_view<double2, 1> A,
-                hc::array_view<double2, 1> B, int tiles,
-                int tile_size) [[hc]] -> void
+template <>
+auto initialize<double4>(hc::tiled_index<1> idx, hc::array_view<double4, 1> A,
+                         hc::array_view<double4, 1> B) [[hc]] -> void
+{
+    auto i = idx.global[0];
+    A[i] = double4{};
+    B[i] = double4{std::numeric_limits<double>::quiet_NaN(),
+                   std::numeric_limits<double>::quiet_NaN(),
+                   std::numeric_limits<double>::quiet_NaN(),
+                   std::numeric_limits<double>::quiet_NaN()};
+}
+
+template <typename DataT>
+auto read_write(hc::tiled_index<1> idx, hc::array_view<DataT, 1> A,
+                hc::array_view<DataT, 1> B, std::size_t elems,
+                unsigned int tiles, unsigned int tile_size) [[hc]] -> void
 {
     auto stride = tiles * tile_size;
     for(auto i = idx.tile[0] * tile_size + idx.local[0];
@@ -56,16 +99,125 @@ auto read_write(hc::tiled_index<1> idx, hc::array_view<double2, 1> A,
     } 
 }
 
-auto write(hc::tiled_index<1> idx, hc::array_view<double2, 1>B, int tiles,
-           int tile_size) [[hc]] -> void
+template <typename DataT>
+auto write(hc::tiled_index<1> idx, hc::array_view<DataT, 1> B,
+           std::size_t elems, unsigned int tiles, unsigned int tile_size) [[hc]]
+-> void
 {
     auto stride = tiles * tile_size;
     for(auto i = idx.tile[0] * tile_size + idx.local[0];
              i < elems;
              i += stride)
     {
-        B[i] = double2{0.0, 0.0};
+        B[i] = DataT{};
     } 
+}
+
+template <typename DataT>
+auto do_benchmark(unsigned int cus, std::ofstream& file) -> void
+{
+    std::cout << "Benchmarking size " << sizeof(DataT) << std::endl;
+    constexpr auto iters = 10;
+    constexpr auto max_mem = 1u << 31; // memory per vector
+    constexpr auto max_elems = max_mem / sizeof(DataT);
+    constexpr auto max_tiles = 1u << 22; // determined experimentally 
+
+    for(auto tile_size = 64u; tile_size <= 1024u; tile_size *= 2u)
+    {
+
+        for(auto elems = tile_size * cus; elems <= max_elems; elems *= 2u)
+        {
+            // Allocate memory on device
+            auto A_d = hc::array_view<DataT, 1>{hc::extent<1>{elems}};
+            auto B_d = hc::array_view<DataT, 1>{hc::extent<1>{elems}};
+
+            for(auto tile_num = cus;
+                     tile_num <= std::min(elems / tile_size, max_tiles);
+                     tile_num *= 2u)
+            {
+                // Initialize device memory
+                hc::parallel_for_each(hc::extent<1>{elems}.tile(1024),
+                [=] (hc::tiled_index<1> idx) [[hc]]
+                {
+                    initialize(idx, A_d, B_d);
+                });
+
+                auto mintime = std::numeric_limits<float>::max();
+                for(auto k = 0; k < iters; ++k)
+                {
+                    auto start = hc::get_system_ticks();
+
+                    hc::parallel_for_each(
+                    hc::extent<1>{tile_num * tile_size}.tile(tile_size),
+                    [=] (hc::tiled_index<1> idx) [[hc]]
+                    {
+                        read_write(idx, A_d, B_d, elems, tile_num, tile_size);
+                    });
+                    
+                    auto stop = hc::get_system_ticks();
+                    auto elapsed_ticks = stop - start;
+
+                    auto tps = hc::get_tick_frequency();
+                    auto tpms = static_cast<float>(tps) / 1000.f;
+
+                    auto elapsed = static_cast<float>(elapsed_ticks) / tpms;
+
+                    mintime = std::min(mintime, elapsed);
+                }
+
+                file << "RW;" << tile_size << ";" << tile_num << ";"
+                     << sizeof(DataT) << ";" << elems << ";"
+                     << mintime << ";"
+                     << (2.0e-9 * sizeof(DataT) * elems) / (mintime / 1e3)
+                     << std::endl;
+            }
+
+            file << std::endl;
+
+            for(auto tile_num = cus;
+                     tile_num <= std::min(elems / tile_size, max_tiles);
+                     tile_num *= 2u)
+            {
+                // Initialize device memory
+                hc::parallel_for_each(hc::extent<1>{elems}.tile(1024),
+                [=] (hc::tiled_index<1> idx) [[hc]]
+                {
+                    initialize(idx, A_d, B_d);
+                });
+
+                auto mintime = std::numeric_limits<float>::max();
+                for(auto k = 0; k < iters; ++k)
+                {
+                    auto start = hc::get_system_ticks();
+
+                    hc::parallel_for_each(
+                    hc::extent<1>{tile_num * tile_size}.tile(tile_size),
+                    [=] (hc::tiled_index<1> idx) [[hc]]
+                    {
+                        write(idx, B_d, elems, tile_num, tile_size);
+                    });
+
+                    auto stop = hc::get_system_ticks();
+                    auto elapsed_ticks = stop - start;
+
+                    auto tps = hc::get_tick_frequency();
+                    auto tpms = static_cast<float>(tps) / 1000.f;
+
+                    auto elapsed = static_cast<float>(elapsed_ticks) / tpms;
+
+                    mintime = std::min(mintime, elapsed);
+                }
+
+                file << "WO;" << tile_size << ";" << tile_num << ";"
+                     << sizeof(DataT) << ";" << elems << ";"
+                     << mintime << ";"
+                     << (1.0e-9 * sizeof(DataT) * elems) / (mintime / 1e3)
+                     << std::endl;
+            }
+
+            file << std::endl;
+        }
+    }
 }
 
 auto main() -> int
@@ -93,81 +245,27 @@ auto main() -> int
         std::exit(EXIT_FAILURE);
     }
 
-    hc::accelerator::set_default(accelerators[index].get_device_path());
+    auto&& my_accelerator = accelerators[index];
+    hc::accelerator::set_default(my_accelerator.get_device_path());
 
-    // Allocate memory on device
-    auto A_d = hc::array_view<double2, 1>{hc::extent<1>{elems}};
-    auto B_d = hc::array_view<double2, 1>{hc::extent<1>{elems}};
+    auto cus = my_accelerator.get_cu_count();
 
-    // Initialize device memory
-    hc::parallel_for_each(hc::extent<1>{elems}.tile(1024),
-    [=] (hc::tiled_index<1> idx) [[hc]]
-    {
-        initialize(idx, A_d, B_d);
-    });
+    auto now = std::chrono::system_clock::now(); 
+    auto cnow = std::chrono::system_clock::to_time_t(now);
 
-    constexpr auto tile_size = 128;
-    constexpr auto num_tiles = (elems + (tile_size - 1)) / tile_size;
-    constexpr auto tiles = num_tiles > 65520 ? 65520 : num_tiles;
+    auto filename = std::stringstream{};
+    filename << std::put_time(std::localtime(&cnow), "%Y-%m-%d %X");
+    filename << ".csv";
 
-    std::cout << "zcopy: operating on vectors of " << elems << " double2s"
-              << " = " << sizeof(double2) * elems << " bytes" << std::endl;
+    auto file = std::ofstream{filename.str()};
 
-    std::cout << "zcopy: using " << tile_size << " tiles per block, "
-              << tiles << " tiles" << std::endl;
+    file << "type;tile_size;tile_num;elem_size;elem_num;mintime;throughput"
+         << std::endl;
 
-    auto mintime = std::numeric_limits<float>::max();
-    for(auto k = 0; k < iters; ++k)
-    {
-        auto start = hc::get_system_ticks();
-
-        hc::parallel_for_each(hc::extent<1>{tiles * tile_size}.tile(tile_size),
-        [=] (hc::tiled_index<1> idx) [[hc]]
-        {
-            read_write(idx, A_d, B_d, tiles, tile_size);
-        });
-        
-        auto stop = hc::get_system_ticks();
-        auto elapsed_ticks = stop - start;
-
-        auto tps = hc::get_tick_frequency();
-        auto tpms = static_cast<float>(tps) / 1000.f;
-
-        auto elapsed = static_cast<float>(elapsed_ticks) / tpms;
-
-        mintime = std::min(mintime, elapsed);
-    }
-
-    std::cout << "RW: mintime = " << mintime << " msec  "
-              << "throughput = "
-              << (2.0e-9 * sizeof(double2) * elems) / (mintime / 1e3)
-              << " GB/sec" << std::endl;
-
-    for(auto k = 0; k < iters; ++k)
-    {
-        auto start = hc::get_system_ticks();
-
-        hc::parallel_for_each(hc::extent<1>{tiles * tile_size}.tile(tile_size),
-        [=] (hc::tiled_index<1> idx) [[hc]]
-        {
-            write(idx, B_d, tiles, tile_size);
-        });
-
-        auto stop = hc::get_system_ticks();
-        auto elapsed_ticks = stop - start;
-
-        auto tps = hc::get_tick_frequency();
-        auto tpms = static_cast<float>(tps) / 1000.f;
-
-        auto elapsed = static_cast<float>(elapsed_ticks) / tpms;
-
-        mintime = std::min(mintime, elapsed);
-    }
-
-    std::cout << "WO: mintime = " << mintime << " msec  "
-              << "throughput = "
-              << (1.0e-9 * sizeof(double2) * elems) / (mintime / 1e3)
-              << " GB/sec" << std::endl;
+    do_benchmark<float>(cus, file);
+    do_benchmark<double>(cus, file);
+    do_benchmark<double2>(cus, file);
+    do_benchmark<double4>(cus, file);
 
     return EXIT_SUCCESS;
 }
--- examples/zcopy/rocm/hip/zcopy.cpp
+++ examples/zcopy/rocm/hip/zcopy.cpp
@@ -1,9 +1,21 @@
 #include <algorithm>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <cstdlib>
 #include <limits>
+#include <sstream>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++17-extensions"
+#pragma clang diagnostic ignored "-Wgnu-anonymous-struct"
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#pragma clang diagnostic ignored "-Wsign-compare"
+#pragma clang diagnostic ignored "-Wunused-parameter"
 #include <hip/hip_runtime.h>
+#pragma clang diagnostic pop
 
 #define CHECK(cmd) \
 { \
@@ -17,11 +29,10 @@
     } \
 } \
 
-constexpr auto elems = 1 << 25;
-constexpr auto iters = 10;
-
-__global__ void read_write(const double2* __restrict__ A,
-                                 double2* __restrict__ B)
+template <typename DataT>
+__global__ void read_write(const DataT* __restrict__ A,
+                                 DataT* __restrict__ B,
+                           std::size_t elems)
 {
     auto stride = gridDim.x * blockDim.x;
     for(auto i = blockIdx.x * blockDim.x + threadIdx.x; i < elems; i += stride)
@@ -30,12 +41,155 @@ __global__ void read_write(const double2* __restrict__ A,
     } 
 }
 
-__global__ void write(double2* __restrict__ B)
+template <typename DataT>
+__global__ void write(DataT* __restrict__ B, std::size_t elems);
+
+template <>
+__global__ void write<float>(float* __restrict__ B, std::size_t elems)
+{
+    auto stride = gridDim.x * blockDim.x;
+    for(auto i = blockIdx.x * blockDim.x + threadIdx.x; i < elems; i += stride)
+    {
+        B[i] = 0.f;
+    }
+}
+
+template <>
+__global__ void write<double>(double* __restrict__ B, std::size_t elems)
+{
+    auto stride = gridDim.x * blockDim.x;
+    for(auto i = blockIdx.x * blockDim.x + threadIdx.x; i < elems; i += stride)
+    {
+        B[i] = 0.0;
+    }
+}
+
+template <>
+__global__ void write<double2>(double2* __restrict__ B, std::size_t elems)
 {
     auto stride = gridDim.x * blockDim.x;
     for(auto i = blockIdx.x * blockDim.x + threadIdx.x; i < elems; i += stride)
     {
         B[i] = make_double2(0.0, 0.0);
+    }
+}
+
+template <>
+__global__ void write<double4>(double4* __restrict__ B, std::size_t elems)
+{
+    auto stride = gridDim.x * blockDim.x;
+    for(auto i = blockIdx.x * blockDim.x + threadIdx.x; i < elems; i += stride)
+    {
+        B[i] = make_double4(0.0, 0.0, 0.0, 0.0);
+    }
+}
+
+
+template <typename DataT>
+auto do_benchmark(int sms, int max_blocks, std::ofstream& file) -> void
+{
+    std::cout << "Benchmarking size " << sizeof(DataT) << std::endl;
+
+    constexpr auto iters = 10;
+    constexpr auto max_mem = 1u << 31; // mem per vector
+    constexpr auto max_elems = static_cast<int>(max_mem / sizeof(DataT));
+
+    for(auto block_size = 64; block_size <= 1024; block_size *= 2)
+    {
+        for(auto elems = block_size * sms; elems <= max_elems; elems *= 2)
+        {
+            // Allocate memory on device
+            auto A_d = static_cast<DataT*>(nullptr);
+            auto B_d = static_cast<DataT*>(nullptr);
+
+            CHECK(hipMalloc(&A_d, sizeof(DataT) * elems));
+            CHECK(hipMalloc(&B_d, sizeof(DataT) * elems));
+
+            for(auto block_num = sms;
+                     block_num <= std::min(elems / block_size, max_blocks);
+                     block_num *= 2)
+            {
+                // Initialize device memory
+                CHECK(hipMemset(A_d, 0x00, sizeof(DataT) * elems)); // zero
+                CHECK(hipMemset(B_d, 0xff, sizeof(DataT) * elems)); // NaN
+
+                auto mintime = std::numeric_limits<float>::max();
+                for(auto k = 0; k < iters; ++k)
+                {
+                    auto start_event = hipEvent_t{};
+                    auto stop_event = hipEvent_t{};
+                    CHECK(hipDeviceSynchronize());
+                    CHECK(hipEventCreate(&start_event));
+                    CHECK(hipEventCreate(&stop_event));
+
+                    CHECK(hipEventRecord(start_event, 0));
+                    hipLaunchKernelGGL(read_write,
+                                       dim3(block_num), dim3(block_size), 0, 0,
+                                       A_d, B_d, elems);
+                    CHECK(hipEventRecord(stop_event, 0));
+
+                    CHECK(hipEventSynchronize(stop_event));
+
+                    auto elapsed = float{};
+                    CHECK(hipGetLastError());
+                    CHECK(hipEventElapsedTime(&elapsed,
+                                              start_event, stop_event));
+
+                    mintime = std::min(mintime, elapsed);
+                }
+
+                file << "RW;" << block_size << ";" << block_num << ";"
+                     << sizeof(DataT) << ";" << elems << ";"
+                     << mintime << ";"
+                     << (2.0e-9 * sizeof(DataT) * elems) / (mintime / 1e3)
+                     << std::endl;
+            }
+
+            file << std::endl;
+
+            for(auto block_num = sms;
+                     block_num <= std::min(elems / block_size, max_blocks);
+                     block_num *= 2)
+            {
+                // Initialize device memory
+                CHECK(hipMemset(B_d, 0xff, sizeof(DataT) * elems)); // NaN
+
+                auto mintime = std::numeric_limits<float>::max();
+                for(auto k = 0; k < iters; ++k)
+                {
+                    auto start_event = hipEvent_t{};
+                    auto stop_event = hipEvent_t{};
+                    CHECK(hipDeviceSynchronize());
+                    CHECK(hipEventCreate(&start_event));
+                    CHECK(hipEventCreate(&stop_event));
+
+                    CHECK(hipEventRecord(start_event, 0));
+                    hipLaunchKernelGGL(write,
+                                       dim3(block_num), dim3(block_size), 0, 0,
+                                       B_d, elems);
+                    CHECK(hipEventRecord(stop_event, 0));
+
+                    CHECK(hipEventSynchronize(stop_event));
+
+                    auto elapsed = float{};
+                    CHECK(hipGetLastError());
+                    CHECK(hipEventElapsedTime(&elapsed, start_event, stop_event));
+
+                    mintime = std::min(mintime, elapsed);
+                }
+
+                file << "WO;" << block_size << ";" << block_num << ";"
+                     << sizeof(DataT) << ";" << elems << ";"
+                     << mintime << ";"
+                     << (1.0e-9 * sizeof(DataT) * elems) / (mintime / 1e3)
+                     << std::endl;
+            }
+
+            file << std::endl;
+
+            CHECK(hipFree(B_d));
+            CHECK(hipFree(A_d));
+        }
     }
 }
 
@@ -46,7 +200,7 @@ auto main() -> int
     CHECK(hipGetDeviceCount(&dev_count));
 
     std::cout << "Available accelerators: " << std::endl;
-    for(auto i = 0u; i < dev_count; ++i)
+    for(auto i = 0; i < dev_count; ++i)
     {
         auto prop = hipDeviceProp_t{};
         CHECK(hipGetDeviceProperties(&prop, i));
@@ -69,80 +223,28 @@ auto main() -> int
     CHECK(hipSetDevice(index));
     CHECK(hipFree(nullptr));
 
-    // Allocate memory on device
-    auto A_d = static_cast<double2*>(nullptr);
-    auto B_d = static_cast<double2*>(nullptr);
+    auto prop = hipDeviceProp_t{};
+    CHECK(hipGetDeviceProperties(&prop, index));
+    
+    auto sms = prop.multiProcessorCount;
+    auto max_blocks = prop.maxGridSize[0];
 
-    CHECK(hipMalloc(&A_d, sizeof(double2) * elems));
-    CHECK(hipMalloc(&B_d, sizeof(double2) * elems));
+    auto now = std::chrono::system_clock::now();
+    auto cnow = std::chrono::system_clock::to_time_t(now);
 
-    // Initialize device memory
-    CHECK(hipMemset(A_d, 0x00, sizeof(double2) * elems)); // zero
-    CHECK(hipMemset(B_d, 0xff, sizeof(double2) * elems)); // NaN
+    auto filename = std::stringstream{};
+    filename << std::put_time(std::localtime(&cnow), "%Y-%m-%d %X");
+    filename << ".csv";
 
-    constexpr auto block_size = 128;
-    constexpr auto num_blocks = (elems + (block_size - 1)) / block_size;
-    constexpr auto blocks = num_blocks > 65520 ? 65520 : num_blocks;
+    auto file = std::ofstream{filename.str()};
 
-    std::cout << "zcopy: operating on vectors of " << elems << " double2s"
-              << " = " << sizeof(double2) * elems << " bytes" << std::endl;
+    file << "type;block_size;block_num;elem_size;elem_num;mintime;throughput"
+         << std::endl;
 
-    std::cout << "zcopy: using " << block_size << " threads per block, "
-              << blocks << " blocks" << std::endl;
-
-    auto mintime = std::numeric_limits<float>::max();
-    for(auto k = 0; k < iters; ++k)
-    {
-        auto start_event = hipEvent_t{};
-        auto stop_event = hipEvent_t{};
-        CHECK(hipDeviceSynchronize());
-        CHECK(hipEventCreate(&start_event));
-        CHECK(hipEventCreate(&stop_event));
-
-        CHECK(hipEventRecord(start_event, 0));
-        hipLaunchKernelGGL(read_write, dim3(blocks), dim3(block_size), 0, 0,
-                           A_d, B_d);
-        CHECK(hipEventRecord(stop_event, 0));
-
-        CHECK(hipEventSynchronize(stop_event));
-
-        auto elapsed = float{};
-        CHECK(hipGetLastError());
-        CHECK(hipEventElapsedTime(&elapsed, start_event, stop_event));
-
-        mintime = std::min(mintime, elapsed);
-    }
-
-    std::cout << "RW: mintime = " << mintime << " msec  "
-              << "throughput = "
-              << (2.0e-9 * sizeof(double2) * elems) / (mintime / 1e3)
-              << " GB/sec" << std::endl;
-
-    for(auto k = 0; k < iters; ++k)
-    {
-        auto start_event = hipEvent_t{};
-        auto stop_event = hipEvent_t{};
-        CHECK(hipDeviceSynchronize());
-        CHECK(hipEventCreate(&start_event));
-        CHECK(hipEventCreate(&stop_event));
-
-        CHECK(hipEventRecord(start_event, 0));
-        hipLaunchKernelGGL(write, dim3(blocks), dim3(block_size), 0, 0, B_d);
-        CHECK(hipEventRecord(stop_event, 0));
-
-        CHECK(hipEventSynchronize(stop_event));
-
-        auto elapsed = float{};
-        CHECK(hipGetLastError());
-        CHECK(hipEventElapsedTime(&elapsed, start_event, stop_event));
-
-        mintime = std::min(mintime, elapsed);
-    }
-
-    std::cout << "WO: mintime = " << mintime << " msec  "
-              << "throughput = "
-              << (1.0e-9 * sizeof(double2) * elems) / (mintime / 1e3)
-              << " GB/sec" << std::endl;
+    do_benchmark<float>(sms, max_blocks, file);
+    do_benchmark<double>(sms, max_blocks, file);
+    do_benchmark<double2>(sms, max_blocks, file);
+    do_benchmark<double4>(sms, max_blocks, file);
 
     return EXIT_SUCCESS;
 }
